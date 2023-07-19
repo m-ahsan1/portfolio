9db8a3ef5b1cda0d5bdb9b897841ff59cfdfb3cd
Muhammad Ahsan <ahsank0811@gmail.com>
2023-07-19 06:01:58 +0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Projects from './components/Projects'
+import Contact from './components/Contact';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
