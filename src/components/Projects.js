fbfecafe3f44c30c7049b76f96b3b6c496eadf63
Muhammad Ahsan <ahsank0811@gmail.com>
2023-07-21 01:50:01 +0500

    fixed portfolio screen

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,24 +5,70 @@ function Projects(props) {
   return (
     <div
       name="project"
-      className="h-screen w-full flex flex-col justify-center bg-gradient-to-b from-black to-gray-800 text-white"
+      className="md:h-screen w-full bg-gradient-to-b from-black to-gray-800 text-white"
     >
-      <div>
-        <h1 className="font-bold text-5xl inline border-b-4 border-gray-500">
-          Projects
-        </h1>
-        <p className="py-6">Here are some of the project that I did.</p>
-      </div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        <div>
-          <img
-            className="rounded-md duration-200 hover:scale-105"
-            src={i}
-            alt=""
-          />
-          <div className="flex justify-around items-center bg-black rounded-md h-10">
-            <button className="hover:scale-105 duration:200">Demo</button>
-            <button className="hover:scale-105 duration:200">Code</button>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <h1 className="font-bold text-5xl inline border-b-4 border-gray-500">
+            Projects
+          </h1>
+          <p className="py-6">Here are some of the project that I did.</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          <div>
+            <img
+              className="rounded-md duration-200 hover:scale-105"
+              src={i}
+              alt=""
+            />
+            <div className="flex justify-around items-center bg-black rounded-md h-10">
+              <button className="hover:scale-105 duration:200">Demo</button>
+              <button className="hover:scale-105 duration:200">Code</button>
+            </div>
+          </div>
+          <div>
+            <img
+              className="rounded-md duration-200 hover:scale-105"
+              src={i}
+              alt=""
+            />
+            <div className="flex justify-around items-center bg-black rounded-md h-10">
+              <button className="hover:scale-105 duration:200">Demo</button>
+              <button className="hover:scale-105 duration:200">Code</button>
+            </div>
+          </div>
+          <div>
+            <img
+              className="rounded-md duration-200 hover:scale-105"
+              src={i}
+              alt=""
+            />
+            <div className="flex justify-around items-center bg-black rounded-md h-10">
+              <button className="hover:scale-105 duration:200">Demo</button>
+              <button className="hover:scale-105 duration:200">Code</button>
+            </div>
+          </div>
+          <div>
+            <img
+              className="rounded-md duration-200 hover:scale-105"
+              src={i}
+              alt=""
+            />
+            <div className="flex justify-around items-center bg-black rounded-md h-10">
+              <button className="hover:scale-105 duration:200">Demo</button>
+              <button className="hover:scale-105 duration:200">Code</button>
+            </div>
+          </div>
+          <div>
+            <img
+              className="rounded-md duration-200 hover:scale-105"
+              src={i}
+              alt=""
+            />
+            <div className="flex justify-around items-center bg-black rounded-md h-10">
+              <button className="hover:scale-105 duration:200">Demo</button>
+              <button className="hover:scale-105 duration:200">Code</button>
+            </div>
           </div>
         </div>
       </div>
